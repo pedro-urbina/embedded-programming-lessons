d5af1b6e392e943a7652df611484eb117120f8e5
Pedro Urbina <purbina14@gmail.com>
2020-02-20 22:14:49 -0500

    Lesson 4 complete

--- lesson1/main.c
+++ lesson1/main.c
@@ -1,14 +1,19 @@
-int counter = 0;
 
 int main() {
-    int *p_int;
-    p_int = &counter;
-    while (*p_int < 21) {
-        ++(*p_int);
-    }
-    
-    p_int = (int *)0x20000002U;
-    *p_int = 0xDEADBEEF;
-    
+    unsigned int counter = 0;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
     return 0;
 }
---
+++ lesson4/main.c
@@ -0,0 +1,25 @@
+int main() {
+    *((unsigned int *)0x400FE608U) = 0x20U;
+    *((unsigned int *)0x40025400U) = 0x0EU;
+    *((unsigned int *)0x4002551CU) = 0x0EU;
+    
+    *((unsigned int *)0x400253FCU) = 0x02U;
+    *((unsigned int *)0x400253FCU) = 0x00U;
+    
+    /*while (1) {      
+        *((unsigned int *)0x400253FCU) = 0x02U;
+        
+        int counter = 0;
+        while (counter < 1000) {
+            ++counter;
+        }
+        
+        *((unsigned int *)0x400253FCU) = 0x00U;
+        counter = 0;
+        while (counter < 1000) {
+          ++counter;
+        }
+    }*/
+    
+    return 0;
+}
